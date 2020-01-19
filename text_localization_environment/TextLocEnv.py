48a5497476927d63331139d20604096b37db5e40
Eric Fischer <eric.fischer95@hotmail.de>
2020-01-19 13:01:46 +0100

    config 2

--- text_localization_environment/TextLocEnv.py
+++ text_localization_environment/TextLocEnv.py
@@ -21,7 +21,7 @@ class TextLocEnv(gym.Env):
     # p: Probability for masking a bounding box in a new observation (applied separately to boxes 0..N-1 during premasking)
     P_MASK = 0.5
     # Reward for next image trigger action
-    ETA2 = 10.0
+    ETA2 = 8.0
 
     def __init__(self, image_paths, true_bboxes, gpu_id=-1,
         playout_episode=False, premasking=True, mode='train',
