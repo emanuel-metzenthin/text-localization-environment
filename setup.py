57e246353743f2b98fd75ac1d30777011529876c
Emanuel Metzenthin <emanuel.metzenthin@student.hpi.de>
2021-05-05 11:48:50 +0200

    Revert "Test name w/o underscore"
    This reverts commit 340a32f7ed090a0909c0deb22cdc5711315671a7.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requirements = [
       'numpy',
       'Pillow']
 
-setup(name='textlocalizationenvironment',
+setup(name='text_localization_environment',
       version='0.1.0',
       install_requires=requirements
 )
